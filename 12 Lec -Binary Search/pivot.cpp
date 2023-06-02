ab99a2a44e5c6ebb0a00ec3a6f77386503113193
Amir Ruzdan <gh5322@myamu.ac.in>
2023-06-02 18:32:04 +0530

    pivot element committed

--- 12 Lec -Binary Search/pivot.cpp
+++ 12 Lec -Binary Search/pivot.cpp
@@ -1,22 +1,30 @@
 #include<iostream>
 using namespace std;
 
-int pivot()
+int pivot(int array[], int n)
 {
-    //commit
+    int i = 0;
+    int j = n-1;
+    int mid = i+(j-i)/2;
+    while (i<j)
+    {
+        if ( array[mid]>=array[0])
+        {
+            i = mid+1;
+        }
+        else
+        {
+            j = mid;
+        }
+        mid = i + (j-i)/2;
+    }
+    return i;
+
 }
-
-
-
-
-
-
-
 
 int main()
 {
-
-
-
+int arr[] = {8,10,17,1,3};
+cout<<"The pivot element is "<<pivot(arr,5)<<endl;
     return 0;
 }
