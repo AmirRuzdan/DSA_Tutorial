a9dc6183bd2bb163e9e33cd3160204b81649680e
Amir Ruzdan <gh5322@myamu.ac.in>
2023-06-10 12:01:00 +0530

    wtf

---
+++ 19 STL/priority_queue.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main() 
+{
+    //max heap based
+    priority_queue<int> maxi;     //maxi named max heap
+
+    //min heap based
+    priority_queue<int, vector<int>,greater<int>> mini;  //mini named min heap
+
+    return 0;
+}
