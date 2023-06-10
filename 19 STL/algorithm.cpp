5df4fde906d45ce63950b3ce6736251b9a136087
Amir Ruzdan <gh5322@myamu.ac.in>
2023-06-10 17:39:26 +0530

    hello

--- 19 STL/algorithm.cpp
+++ 19 STL/algorithm.cpp
@@ -6,13 +6,17 @@ using namespace std;
 int main() 
 {
     vector<int> v;
-    v.push_back(5);
-    v.push_back(4);
     v.push_back(1);
+    v.push_back(3);
+    v.push_back(6);
     v.push_back(9);
-    v.push_back(0);
-    cout<<binary_search(v.begin(),v.end(),6)<<endl;
-    cout<<binary_search(v.begin(),v.end(),9)<<endl;
+    v.push_back(10);
+    cout<<"Finding element 6 = "<<binary_search(v.begin(),v.end(),6)<<endl;    // it only tells if ELEMENT EXISTS OR not
+    // cout<<binary_search(v.begin(),v.end(),9)<<endl;
+    //Finding the index of element using iterator
+
+
+    cout<<"Lower bound = "<<lower_bound(v.begin(),v.end(),6)-v.begin()<<endl;
 
 return 0;
 }
