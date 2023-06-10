4c9a443809fa0951d64aab9f7dd25c64ed7a2c6d
Amir Ruzdan <gh5322@myamu.ac.in>
2023-06-10 22:56:10 +0530

    hello

--- 19 STL/algorithm.cpp
+++ 19 STL/algorithm.cpp
@@ -17,6 +17,26 @@ int main()
 
 
     cout<<"Lower bound = "<<lower_bound(v.begin(),v.end(),6)-v.begin()<<endl;
+    cout<<"Upper bound = "<<upper_bound(v.begin(),v.end(),4)-v.begin()<<endl;
+
+
+    int a =3;
+    int b = 5;
+    cout<<"max = "<<max(a,b)<<endl;
+    cout<<"min = "<<min(a,b)<<endl;
+    swap(a,b);
+    cout<<"a ="<<a<<endl;
+    string abcd = "abcd";
+    reverse(abcd.begin(),abcd.end() );
+    cout<<"Reversed String "<<abcd<<endl;
+    //Rotating a vector about a point v ={ 1,3,6,9,10}
+    rotate(v.begin(), v.begin()+1,v.end());  //rotate about 3
+
+
+    for(int i:v)
+    {
+        cout<<i<<" ";
+    }
 
 return 0;
 }
