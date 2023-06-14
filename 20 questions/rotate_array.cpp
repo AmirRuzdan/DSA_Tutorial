a4837e775b8448c0d6e3c7073fd38f2d28cc2a12
Amir Ruzdan <gh5322@myamu.ac.in>
2023-06-14 22:59:22 +0530

    commit

---
+++ 20 questions/rotate_array.cpp
@@ -0,0 +1,32 @@
+// Rotate Array
+// input = {3,5,7,9,11} 
+//output = { 9,11,3,5,7} for k = 2
+//% mod function arr[(i+k)%n] = arr[i]
+
+
+#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void rotate( vector<int>& nums , int k)   ///int k is the pivot value
+{
+    int n = nums.size();
+    vector<int> temp(n);
+    for (int i = 0; i < n; i++)
+    {
+        temp[(i+k)%n]
+        ]
+    }
+
+}
+
+
+
+int main() 
+{   
+
+
+
+return 0;
+}
