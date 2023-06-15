988d825b6fdfcda84d7aa3fbc29889fb9305523a
Amir Ruzdan <gh5322@myamu.ac.in>
2023-06-15 20:35:36 +0530

    committed

--- 20 questions/array_sum.cpp
+++ 20 questions/array_sum.cpp
@@ -1,23 +1,76 @@
 // Add two arrays 
-// input = 1234 ; 5;
-// output = 1239
-
-
+// input =  1238 ; 
+        ///+   5;
+// output = 1243
 #include <bits/stdc++.h>
 #include<iostream>
+#include<vector>
 using namespace std;
+vector<int> reverse( vector<int> v)
+{
+    int i = 0;
+    int j = v.size()-1;
+    while( i<j)
+    {
+        swap(v[i++], v [j--]);
+    }
+    return v;
+}
+vector<int> arraysum( vector<int> a , int n , vector<int> b , int m)
+{
+    int i = n-1; 
+    int j  = m-1;
+    int carry= 0 ;
+    vector<int> ans;
+    while (i>=0 && j >= 0 )
+    {
+        int val1 = a[i];
+        int val2 = b[j];
+        int sum = val1 + val2 + carry;
+        carry = sum/10;
+        int value = sum%10;
+        ans.push_back(value);
+        i--;
+        j--; 
+    }
+        while (i>=0  )
+    {
+        int val1 = a[i];
+        int sum = val1  + carry;
+        carry = sum/10;
+        int value = sum%10;
+        ans.push_back(value);
+        i--;
+    }
+        while (j >= 0 )
+    {
+        int val2 = b[j];
+        int sum =  val2 + carry;
+        carry = sum/10;
+        int value = sum%10;
+        ans.push_back(value);
+        j--; 
+    }
+    while(carry!=0)
+    {
+        int sum = carry;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        j--;
 
-
-
+    }
+    return reverse(ans) ;
+    
+    
+}
 
 
 
 int main() 
 {
-    int array1[2] = {1,2};
-    int array2[2] ={2,3};
-    int answer, num1 , num2 ;
-   
-
+    vector<int> vec1 = { 2,3,4,5};
+    vector<int> vec2 = { 2,3,4,5};
+    arraysum(vec1,4, vec2,4);
 return 0;
 }
