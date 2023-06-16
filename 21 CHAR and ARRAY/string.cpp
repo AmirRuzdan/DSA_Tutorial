de548a69f5fd9eb9b72921597f148980eb3ae78d
Amir Ruzdan <gh5322@myamu.ac.in>
2023-06-16 20:43:00 +0530

    comiitii

---
+++ 21 CHAR and ARRAY/string.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <string>
+#include<iostream>
+using namespace std;
+int main() 
+{
+    string s;
+    string str = "Amir";
+    cout<<str.length();
+    str.push_back
+
+
+return 0;
+}
