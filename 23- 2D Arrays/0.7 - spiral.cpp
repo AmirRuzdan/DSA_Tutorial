cfcdede977d047cc84f67bf8d84f3a038319902a
Amir Ruzdan <gh5322@myamu.ac.in>
2023-06-27 18:13:18 +0530

    spiral.cpp

--- 23- 2D Arrays/0.7 - spiral.cpp
+++ 23- 2D Arrays/0.7 - spiral.cpp
@@ -4,7 +4,47 @@ using namespace std;
 
 vector<int> spiral(vector<vector<int>>& matrix)
 {
+    vector<int> ans;
+    int row = matrix.size();              // no. of rows
+    int column = matrix[0].size();        // no. of columns
+    int total = row*column;               // total number of elements
 
+
+    //initialising index of matrix
+    int startingRow = 0;
+    int startingCol= 0;
+    int endingRow = row- 1;
+    int endingCol = column - 1;
+    int count = 0;
+    while (count<total)
+    {
+        for (int  i = startingCol; count<total && i <= endingCol; i++)
+        {
+            ans.push_back(matrix[startingRow][i]);
+            count++;
+        }
+        startingRow++;
+        for (int i = startingRow;count<total &&  i <= endingRow; i++)
+        {
+            ans.push_back(matrix[i][endingCol]);
+            count++;
+        }
+        endingCol--;
+        for (int  i = endingCol;count<total &&  i >=startingCol ; i--)
+        {
+            ans.push_back(matrix[endingRow][i]);
+            count++;
+        }
+        endingRow--;
+        for (int  i =endingRow; i >=startingRow; i--)
+        {
+            ans.push_back(matrix[i][startingCol]);
+            count++;
+        }
+        startingCol++;
+        
+    }
+    return ans;
 }
 
 
@@ -13,8 +53,23 @@ vector<int> spiral(vector<vector<int>>& matrix)
 int main() 
 {
 
-vector<vector<int>> myarray = {1,2,3,4,5,6,7,8,9};
-printspiral();
+vector<vector<int>> myarray = {{1,2,3},{4,5,6},{7,8,9}};
+
+spiral(myarray);
+
+
+
+
+    vector<int> result = spiral(myarray);
+
+    for (int i = 0; i < result.size(); i++) 
+    {
+      
+
+        cout << result[i] << " ";
+        
+    
+    }
 
 
 
