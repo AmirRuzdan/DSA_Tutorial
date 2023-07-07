b41256b1de11dc6b4d229ababcfea057efbffb20
AmirRuzdan <gh5322@myamu.ac.in>
2023-07-07 10:40:27 +0530

    dd

--- 25- Pointers/1-Introduction.cpp
+++ 25- Pointers/1-Introduction.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() 
 {
     int num = 5;
+<<<<<<< Updated upstream
     int *ptr = &num;
 
 
@@ -12,6 +13,18 @@ int main()
     cout<<"The value (*ptr) is = "<<*ptr<<endl;
     cout<<"*ptr stores value of num"<<*ptr<<endl;
     cout<<"The value of ptr is = "<<ptr<<endl;
+=======
+    int i = 10;
+    cout<<num<<endl;
+    int *ptr = &num;
+    // address of operator --> &
+    cout<<"Address of num is (&num) ="<< &num <<endl;       // pointers stores address data
+    cout<<"Value of *ptr is  ="<< *ptr <<endl;     // value stored at address ptr is *ptr  
+    cout<<"Value of ptr is  ="<< ptr <<endl;       // address of num is ptr
+    cout<<"Value of num is  ="<< num <<endl;       // address of num is ptr
+
+   
+>>>>>>> Stashed changes
 
 return 0;
-}
+} 
