08d951106cac7a0569376e055df7b20aed4e30af
Amir Ruzdan <gh5322@myamu.ac.in>
2023-07-08 00:04:01 +0530

    comoyjirgt

--- 25- Pointers/4- pointer-characters.cpp
+++ 25- Pointers/4- pointer-characters.cpp
@@ -5,15 +5,15 @@ int main()
 {
     int arr[5] = {1,2,3,4,5};
     char ch[6] = "abcde";    //extra null character is always present in strings at the end
-    cout<<"Only typing arrwill give us the address of first cell "<<arr<<endl;         // prints the address of the first location of the array
+    cout<<"Only typing arr will give us the address of first cell ="<<arr<<endl;         // prints the address of the first location of the array , which is also the adress of the array
     cout<<ch<<endl;           // cout function is different for int array and char array hebce it outputs the string for the char data type
     
 
     char *c = &ch[0];     
-    cout<<c <<endl;   // this will prints entire string
+    cout<<"Address of character ch = "<< c <<endl;   // this will prints entire string
 
 
-    char temp = 'z'; // it will keep printing until it finds a null character
+    char temp = 'm'; // it will keep printing until it finds a null character
     char *p = &temp;
     cout<<p<<endl;
 
