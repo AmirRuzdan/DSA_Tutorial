46399ec0f671f1721f8431c50a9c7204924ff075
Amir Ruzdan <gh5322@myamu.ac.in>
2023-07-10 00:58:09 +0530

    yellow

--- 25- Pointers/5- functions_pointer.cpp
+++ 25- Pointers/5- functions_pointer.cpp
@@ -2,25 +2,27 @@
 #include<iostream>
 using namespace std;
 
-void print( int *p)
-{
-    cout<<*p<<endl;
 
-}
-
-
+// void print(int *p )
+// {
+//     cout<<*p<<endl;
+// }
 
 void update(int *p)
 {
     p = p+1;
+    cout<<"Inside value = "<<p<<endl;
+    *p = *p +1;
 }
+
+
 int main() 
 {
     int value = 5;
     int *p = &value;
-
-
     // print(p);
+
+    
 
     cout<<"Before"<<p<<endl;
     update(p);
