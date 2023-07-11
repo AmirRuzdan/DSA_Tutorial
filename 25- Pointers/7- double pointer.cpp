df24c2b079fdd19c507ef72203dae05e51ba4f30
Amir Ruzdan <gh5322@myamu.ac.in>
2023-07-11 21:06:12 +0530

    gfgfgg

--- 25- Pointers/7- double pointer.cpp
+++ 25- Pointers/7- double pointer.cpp
@@ -1,12 +1,47 @@
 #include <bits/stdc++.h>
 #include<iostream>
 using namespace std;
+
+void update(int **p2)
+{
+    // p = p+1;     -- >no changes occur
+
+    //*p2 = *p2 +1;   --> value of ptr changes
+
+     **p2 = **p2 + 1;  // --> this will change 
+}
+
+
 int main() 
 
-
-
-
 {
+    int num = 5;
+    int *ptr = &num;
+    int **ptr2 = &ptr;
+    
+
+    // cout<<"different ways to print 5  "<<endl;
+    // cout<<num<<endl;
+    // cout<<*ptr<<endl;
+    // cout<<**ptr2<<endl;
+    // cout<<"different ways to print ptr  "<<endl;
+    // cout<<&num<<endl;
+    // cout<<ptr<<endl;
+    // cout<<*ptr2<<endl;
+    // cout<<"different ways to print ptr2  "<<endl;
+    // cout<<&ptr<<endl;
+    // cout<<ptr2<<endl;
+
+    cout<<endl<<endl;
+    cout<<"before ="<<num<<endl;
+    cout<<"before ="<<ptr<<endl;
+    cout<<"before ="<<ptr2<<endl;
+
+    update(ptr2);
+
+    cout<<"after ="<<num<<endl;
+    cout<<"after ="<<ptr<<endl;
+    cout<<"after ="<<ptr2<<endl;
 
 
 return 0;
