093daf424f0b6ca7f0e6050289e27934d204afe9
AmirRuzdan <gh5322@myamu.ac.in>
2023-08-07 00:09:38 +0530

    comitit

--- 26- Recursion/10-linear_search_recursion.cpp
+++ 26- Recursion/10-linear_search_recursion.cpp
@@ -2,30 +2,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool  is_found ( int *arr , int size, int key )
-{
-    
+bool  is_found ( int arr[] , int size, int key )
+
+{   
+    if(size==0)
+    {
+        return false  ;
+
+    }
+    if( arr[0]== key)
+    {
+        return true;
+    }
+    else 
+    {
+        bool ans = is_found(arr+1,size -1 , key );
+        return ans ;
+    }
 }
-
-
 
 
 int main()
 {
-    int array[5] = {2,3,4,5,6};
+    int array[6] = {2,3,4,5,6,77};
+    int size = 6 ;
     int key;
     cout<<"Enter your key element =";
     cin>>key;
-    int ans = is_found( array, 5, key);
+    bool ans = is_found( array, size, key);
     if ( ans )
     {
-        cout<<"element is present int the array"<<endl;
+        cout<<"element is present in the array"<<endl;
     }
     else
     {
         cout<<"Element not found"<<endl;
     }
-    
+
 
 return 0;
 }
---
+++ 26- Recursion/11-binary_search_recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int binary_search(int arr[], int size , int key)
+{
+    if (int size ==0 )
+{
+    return  
+}
+}
+
+
+
+int main()
+{
+
+
+
+
+return 0;
+}
