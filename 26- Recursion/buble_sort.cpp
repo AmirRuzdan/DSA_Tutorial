4914fb8844f1dcbd2d0465e314cd868bae9bda52
Amir Ruzdan <gh5322@myamu.ac.in>
2023-08-29 21:17:18 +0530

    igfjijih

---
+++ 26- Recursion/buble_sort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
+#include<array>
+using namespace std;
+
+//PRINTING ARRAY
+void print(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+    cout<<array[i]<<" "; 
+    }
+}
+
+void bubble_srt( int a[], int size)
+{
+    for (int i = 0; i < size-1; i++)
+    {
+        
+        for (int j = i+1; j <size; j++)
+        {
+            if(a[i]>a[j])
+            {
+                swap(a[i],a[j]);
+            }
+            
+        }
+        
+    }
+}
+
+
+int main() 
+{
+
+    int array[5] = {9,6,4,21,5};
+
+    bubble_srt(array,5);
+    print(array, 5);
+
+
+return 0;
+}
