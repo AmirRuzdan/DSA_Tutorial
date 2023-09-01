96d897d7906c6219a569f3f1fc4be0080c42807a
Amir Ruzdan <gh5322@myamu.ac.in>
2023-09-01 21:11:14 +0530

    coomitr

---
+++ 26- Recursion/17-quicksort.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+void print( int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<< " ";
+    }
+    
+}
+
+int partition(int *arr, int start , int end)
+{
+    int pivot = arr[start];
+
+    // finding the right place of pivot using count
+    int count = 0 ;
+    for (int i = start+1; i<= end; i++)
+    {
+        if(arr[i]<=pivot)
+        {
+            count++;
+        }
+    }
+    int pivotindex = start + count;
+    swap( arr[pivotindex], arr[start]);
+    
+
+    int i = start;
+    int j = end;
+
+    while ( i<pivotindex && j>pivotindex)
+    {
+        while (arr[i]<pivot)
+        {
+            i++;
+        }
+        while (arr[j]>pivot)
+        {
+            j--;
+        }
+
+        if (i<pivotindex && j>pivotindex)
+        {
+            swap(arr[i++],arr[j--]);
+        
+        }
+        
+        
+        
+    }
+    
+}
+
+
+void quicksort ( int *arr , int  start, int end)
+
+{
+     // base case
+    if (start>=end)
+    {
+        return;
+    }
+
+    int p = partition(arr, start, end);
+
+    // recursive call to sort left part    
+    quicksort(arr,start , p-1 );
+    // recursive call to sort right part
+    quicksort(arr, p+1, end);
+    
+}
+
+
+
+int main() 
+{
+    int array[5] = {7,6,4,3,1};
+    int n = sizeof(array)/4;
+    print(array, 5);
+    cout<<endl;
+    quicksort(array , 0 , n-1);
+    print(array, 5);
+
+return 0;
+}
