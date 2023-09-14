de1a5e8a8e173fc8f2951cb5720fc6323c8c6f29
Amir Ruzdan <gh5322@myamu.ac.in>
2023-09-15 00:33:49 +0530

    jjj

---
+++ 40 - OOPS/1-classes.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+class Hero {
+
+
+    // properties
+    // char name[100];
+    int health;
+    // char level;
+
+
+
+
+
+};
+
+int main() 
+{
+    //creation of object of type(class) hero
+    Hero spiderman;
+    cout<<"size of "<<sizeof(spiderman)<<endl;
+
+return 0;
+}
