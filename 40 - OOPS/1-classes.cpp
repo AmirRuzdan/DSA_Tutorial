1fdbab30ec35b07557799030b78279a6d54aaa2c
Amir Ruzdan <gh5322@myamu.ac.in>
2023-09-15 20:23:13 +0530

    fdfd

--- 40 - OOPS/1-classes.cpp
+++ 40 - OOPS/1-classes.cpp
@@ -1,15 +1,25 @@
 #include <bits/stdc++.h>
 #include<iostream>
-#include "hero.cpp"
 using namespace std;
 
+class Hero {
 
+public:
+    // properties
+        int health;
+        char name[100];
+        int level;
+
+};             
 
 int main() 
 {
     //creation of object of type(class) hero
     Hero spiderman;
-    cout<<"size of "<<sizeof(spiderman)<<endl;
-
+    cout<<"size of spiderman object = "<<sizeof(spiderman)<<endl;
+    spiderman.health = 70;
+    spiderman.level = 9000;
+    cout<<spiderman.health<<endl;
+    cout<<spiderman.level<<endl;
 return 0;
 }
---
+++ 40 - OOPS/3-static vs dynamic allocation.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+class Hero {
+
+private:
+        int health;
+
+public:
+        char level;
+
+int getHealth() 
+{
+    return health;
+}
+char getLevel()
+{
+    return level;
+}
+
+void setHealth(int h)
+{
+    health = h;
+}
+
+void setLevel(char ch)
+{
+    level = ch;
+}
+
+
+
+int main() 
+{
+
+//static allocation
+Hero a;
+cout<<"Level is  = "<<a.level<<endl;                // since level is public
+cout<<"Health is  = "<<a.getHealth()<<endl;        //since health is public
+
+//dynamic allocation
+Hero *b = new Hero;
+cout<<"Level is = "<<(*b).level<<endl;
+cout<<"Health is = "<<(*b).level<<endl;
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
